d379b89487d77d27804b432dd42f032aa27c7546
zoro-diablo <vyshnavk117@gmail.com>
2025-06-30 19:40:28 +0530

    Refactor requireAuth middleware: improve error handling and streamline user validation logic

--- server/middleware/requireAuth.js
+++ server/middleware/requireAuth.js
@@ -10,16 +10,31 @@ const requireAuth = async (req, res, next) => {
     return res.status(401).json({ err: 'Authorization token required' });
   }
 
+  if (!authorization.startsWith('Bearer ')) {
+    return res.status(401).json({ err: 'Invalid authorization format' });
+  }
+
   const token = authorization.split(' ')[1];
 
   try {
     const { _id } = jwt.verify(token, secret);
 
-    req.user = await User.findOne({ _id }).select('_id');
+    const user = await User.findOne({ _id }).select('_id');
+    if (!user) {
+      return res.status(401).json({ err: 'User not found' });
+    }
 
+    req.user = user;
     next();
   } catch (err) {
     console.log(err);
+    
+    if (err.name === 'TokenExpiredError') {
+      return res.status(401).json({ err: 'Token expired' });
+    } else if (err.name === 'JsonWebTokenError') {
+      return res.status(401).json({ err: 'Invalid token' });
+    }
+    
     res.status(401).json({ err: 'Request is not authorized' });
   }
 };
